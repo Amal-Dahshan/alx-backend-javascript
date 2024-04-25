5300903f335d029eae275d8e7f8a6da682a03f89
Amal-Dahshan <amaldahshan11710@gmail.com>
2024-04-25 15:06:32 -0700

    fix

--- 0x02-ES6_classes/2-hbtn_course.js
+++ 0x02-ES6_classes/2-hbtn_course.js
@@ -1,11 +1,12 @@
 export default class HolbertonCourse {
   constructor(name, length, students) {
-	if (typeof(name) !== 'string') {
-		throw new TypeError('Name must be a string');
-	} else {
-		this._name = name;
-	}
-	if (typeof(length) !== 'number') {
+    if (typeof(name) !== 'string') {
+      throw new TypeError('Name must be a string');
+    } else {
+      this._name = name;
+    }
+
+    if (typeof(length) !== 'number') {
       throw new TypeError('Length must be a number');
     } else {
       this._length = length;
@@ -58,4 +59,3 @@ export default class HolbertonCourse {
     this._students = students;
   }
 }
-
